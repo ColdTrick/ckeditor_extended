2d31a80cd59c8bdfadc527ef4e88346f49a3c41b
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-05-08 14:13:55 +0200

    added dutch translation

---
+++ languages/nl.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'ckeditor:settings:editor_config' => "Pas de instellingen van de CKEditor hieronder aan",
+	'ckeditor:settings:link' => "Bekijk op de website van CKEditor alle mogelijke instellingen",
+	'ckeditor:settings:example' => "Voorbeeld van de editor",
+	'ckeditor:settings:example:description' => "De informatie die in deze editor wordt ingevoerd zal niet worden opgeslagen. Veel plezier met het testen.",
+	'ckeditor:settings:image_upload_allowed' => "Sta upload van afbeeldingen in de editor toe",
+	'ckeditor:settings:show_html_toggler' => "Toon de HTML schakel link",
+	'ckeditor:settings:show_html_toggler:option:admin_only' => "Alleen voor beheerders",
+);
