b82d4452dc0e1a04ba468492a136a0612d5bafc3
Jerome Bakker <jeabakker@coldtrick.com>
2014-07-17 09:17:00 +0200

    dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,11 +1,21 @@
 <?php
+/**
+ * This file was created by Translation Editor v2.0
+ * On 2014-07-17 09:16
+ */
 
-return array(
-	'ckeditor_extended:settings:editor_config' => "Pas de instellingen van de CKEditor hieronder aan",
-	'ckeditor_extended:settings:link' => "Bekijk op de website van CKEditor alle mogelijke instellingen",
-	'ckeditor_extended:settings:example' => "Voorbeeld van de editor",
-	'ckeditor_extended:settings:example:description' => "De informatie die in deze editor wordt ingevoerd zal niet worden opgeslagen. Veel plezier met het testen.",
-	'ckeditor_extended:settings:image_upload_allowed' => "Sta upload van afbeeldingen in de editor toe",
-	'ckeditor_extended:settings:show_html_toggler' => "Toon de HTML schakel link",
-	'ckeditor_extended:settings:show_html_toggler:option:admin_only' => "Alleen voor beheerders",
+$language = array (
+  'ckeditor_extended:settings:htmlawed:title' => 'HTMLawed instellingen',
+  'ckeditor_extended:settings:htmlawed:info' => 'Gonfigureer extra regels voor HTMLawed om bepaalde elementen en schema\'s te vertrouwen',
+  'ckeditor_extended:settings:htmlawed:elements' => 'HTML elementen',
+  'ckeditor_extended:settings:htmlawed:schemes' => 'Schema\'s',
+  'ckeditor_extended:upload:no_image' => 'Het opgegeven bestand is geen afbeelding',
+  'ckeditor_extended:settings:editor_config' => 'Pas de instellingen van de CKEditor hieronder aan',
+  'ckeditor_extended:settings:link' => 'Bekijk op de website van CKEditor alle mogelijke instellingen',
+  'ckeditor_extended:settings:example' => 'Voorbeeld van de editor',
+  'ckeditor_extended:settings:example:description' => 'De informatie die in deze editor wordt ingevoerd zal niet worden opgeslagen. Veel plezier met het testen.',
+  'ckeditor_extended:settings:image_upload_allowed' => 'Sta upload van afbeeldingen in de editor toe',
+  'ckeditor_extended:settings:show_html_toggler' => 'Toon de HTML schakel link',
+  'ckeditor_extended:settings:show_html_toggler:option:admin_only' => 'Alleen voor beheerders',
 );
+add_translation("nl", $language);
