dddd3e23e8f30b72b2d9c196405c7b8b5f20617e
Jerôme Bakker <jeabakker@coldtrick.com>
2022-03-11 14:08:40 +0100

    chore: wrapping up v5.0.2

--- elgg-plugin.php
+++ elgg-plugin.php
@@ -11,7 +11,7 @@ if (is_dir(__DIR__ . '/vendor')) {
 
 return [
 	'plugin' => [
-		'version' => '5.0.1',
+		'version' => '5.0.2',
 		'dependencies' => [
 			'ckeditor' => [
 				'position' => 'after',
